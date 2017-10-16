bc81dfbb23bebfa6d4b6aa91f23da88c6bb213df
Le <liu00414@algonquinlive.com>
2017-10-15 21:02:47 -0400

    fifth

--- namespace/namespace.js
+++ namespace/namespace.js
@@ -4,25 +4,25 @@ var LIU00414 = {
         let boxes = document.getElementById('boxes');
         myBox.classList.add('box');
         myBox.textContent = 'LIU00414';
-        boxes.appendChild(d);
+        boxes.appendChild(myBox);
         myBox.addEventListener('click', changecolor);
         myBox.addEventListener('mouseover', togglehighlight);
         myBox.addEventListener('mouseout', togglehighlight);
-console.log('randomColor');
 
-    function togglehighlight(ev) {
-        ev.currentTarget.classList.toggle('highlight');
 
-    }
+        function togglehighlight(ev) {
+            ev.currentTarget.classList.toggle('highlight');
 
-    function changecolor(ev) {
-        function color() {
-            return Math.ceil(Math.random() * 255);
         }
-        let randomColor = 'rgb(' + color() + ',' + color() + ',' + color() + ')';
-        ev.currentTarget.style.borderColor = randomColor;
-        ev.currentTarget.style.backgroundColor = randomColor;
-        console.log('randomColor');
-    }
+
+        function changecolor(ev) {
+            function color() {
+                return Math.ceil(Math.random() * 255);
+            }
+            let randomColor = 'rgb(' + color() + ',' + color() + ',' + color() + ')';
+            ev.currentTarget.style.borderColor = randomColor;
+            ev.currentTarget.style.backgroundColor = randomColor;
+            console.log('randomColor');
+        }
     }
 }
